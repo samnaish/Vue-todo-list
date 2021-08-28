84911940ff41109cf428408403d6e692e5100d01
Sam Naish <sam.naish92@gmail.com>
2021-08-28 17:19:27 +0100

    fix: added files to project repo

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        isEditing: false,
+        selectedIndex: null,
+        todo: '',
+        todos: [],
+    },
+    methods: {
+        storeTodo() {
+            // adds the craeted todo, to the list
+            this.todos.push(this.todos)
+            // clears the input field of text
+            this.todo = ''
+        },
+        editTodo(index, todo) {
+            this.todo = todo
+            this.selectedIndex = index
+            this.isEditing = true
+        },
+        updateTodo() {
+            this.todos.splice(this.selectedIndex, 1, this.todo)
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1)
+        }
+}
+})
